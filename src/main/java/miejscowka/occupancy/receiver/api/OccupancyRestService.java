8cc8e4d3e5f9dca86f8462c9715097d6ae125980
Sebastian <sebastian.lauks13@gmail.com>
2021-12-07 19:35:29 +0100

    Fixed Swagger

--- src/main/java/miejscowka/occupancy/receiver/api/OccupancyRestService.java
+++ src/main/java/miejscowka/occupancy/receiver/api/OccupancyRestService.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-
 public interface OccupancyRestService {
 
     @ApiOperation(value = "Set relative occupancy",
